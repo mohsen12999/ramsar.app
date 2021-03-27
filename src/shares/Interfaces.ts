1fbeb831c9ba4aa7b205f4087b6906cf33ee15fc
mohsen <mohsen1299@gmail.com>
2021-03-27 21:23:00 +0430

    update model in interfaces

--- src/shares/Interfaces.ts
+++ src/shares/Interfaces.ts
@@ -1,2 +1,15 @@
-export interface ICategory {}
-export interface IFacility {}
+export interface ICategory {
+  id: number;
+  name: string;
+  img: string;
+
+  parentId: number;
+}
+
+export interface IFacility {
+  id: number;
+  name: string;
+  tags: string[];
+
+  categoryId: number;
+}
