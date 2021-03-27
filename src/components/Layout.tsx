1b6739acc710a8f61c4d701bbf2e440fefddc7d8
mohsen <mohsen1299@gmail.com>
2021-03-28 01:30:03 +0430

    start making layout page

---
+++ src/components/Layout.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Pages } from "../shares/URLs";
+
+const Layout: React.FC = ({ children }) => (
+  <div className="rounded-lg shadow bg-base-200 drawer h-screen">
+    <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+    <div className="flex flex-col drawer-content">
+      <div className="w-full navbar bg-base-300">
+        <div className="flex-none lg:hidden">
+          <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+        </div>
+        <div className="flex-1 px-2 mx-2">
+          <span>رامسر اپ</span>
+        </div>
+        <div className="flex-none hidden lg:block">
+          <ul className="menu horizontal">
+            <li>
+              <Link to={Pages.HomePage} className="rounded-btn">
+                صفحه نخست
+              </Link>
+            </li>
+            <li>
+              <Link to={Pages.Faq} className="rounded-btn">
+                سوالات متداول
+              </Link>
+            </li>
+            <li>
+              <Link to={Pages.About} className="rounded-btn">
+                درباره ما
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <main>{children}</main>
+    </div>
+    <div className="drawer-side">
+      <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+      <ul className="p-4 overflow-y-auto menu w-80 bg-base-100">
+        <li>
+          <Link to={Pages.HomePage}>صفحه نخست</Link>
+        </li>
+        <li>
+          <Link to={Pages.Faq}>سوالات متداول</Link>
+        </li>
+        <li>
+          <Link to={Pages.About}>درباره ما</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default Layout;
