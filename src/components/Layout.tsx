4f52ceb14fff54c1287f09a06051a9682dbc57fd
mohsen <mohsen1299@gmail.com>
2021-03-29 11:30:48 +0430

    make home better

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC = ({ children }) => (
   <div className="rounded-lg shadow bg-base-200 drawer h-screen">
     <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
     <div className="flex flex-col drawer-content">
-      <div className="w-full navbar bg-base-300">
+      <div className="w-full navbar mb-2 bg-base-300">
         <div className="flex-none lg:hidden">
           <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
             <svg
@@ -27,28 +27,68 @@ const Layout: React.FC = ({ children }) => (
         </div>
         <div className="flex-1 px-2 mx-2 items-center">
           <span className="text-lg font-bold">رامسر اپ</span>
+        </div>
+        <div className="flex-1 px-2 mx-2 items-center">
           <div className="form-control">
             <input
               type="text"
               placeholder="جستجو"
-              className="input input-ghost"
+              className="input input-ghost search-input"
             />
           </div>
         </div>
         <div className="flex-none hidden lg:block">
           <ul className="menu horizontal">
             <li>
-              <Link to={Pages.HomePage} className="rounded-btn">
+              <Link
+                to={process.env.PUBLIC_URL + Pages.HomePage}
+                className="rounded-btn"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  className="inline-block w-5 ml-2"
+                >
+                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                </svg>
                 صفحه نخست
               </Link>
             </li>
             <li>
-              <Link to={Pages.Faq} className="rounded-btn">
+              <Link
+                to={process.env.PUBLIC_URL + Pages.Faq}
+                className="rounded-btn"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-5 ml-2"
+                >
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z"></path>
+                </svg>
                 سوالات متداول
               </Link>
             </li>
             <li>
-              <Link to={Pages.About} className="rounded-btn">
+              <Link
+                to={process.env.PUBLIC_URL + Pages.About}
+                className="rounded-btn"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-5 ml-2"
+                >
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
+                </svg>
                 درباره ما
               </Link>
             </li>
@@ -61,13 +101,48 @@ const Layout: React.FC = ({ children }) => (
       <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
       <ul className="p-4 overflow-y-auto menu w-80 bg-base-100">
         <li>
-          <Link to={Pages.HomePage}>صفحه نخست</Link>
+          <Link to={process.env.PUBLIC_URL + Pages.HomePage}>
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              className="inline-block w-5 ml-2"
+            >
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+            </svg>
+            صفحه نخست
+          </Link>
         </li>
         <li>
-          <Link to={Pages.Faq}>سوالات متداول</Link>
+          <Link to={process.env.PUBLIC_URL + Pages.Faq}>
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 ml-2"
+            >
+              <path d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z"></path>
+            </svg>
+            سوالات متداول
+          </Link>
         </li>
         <li>
-          <Link to={Pages.About}>درباره ما</Link>
+          <Link to={process.env.PUBLIC_URL + Pages.About}>
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 ml-2"
+            >
+              <path d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
+            </svg>
+            درباره ما
+          </Link>
         </li>
       </ul>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Layout from "../components/Layout";
 import { ICategory } from "../shares/Interfaces";
 import { Pages } from "../shares/URLs";
@@ -10,32 +10,43 @@ interface IHomeProps {
   categories: ICategory[];
 }
 
-const Home: React.FC<IHomeProps> = ({ categories }) => (
-  <Layout>
-    <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 md:grid-col-6">
-      {categories
-        .filter((c) => c.parentId === 0)
-        .map((category) => (
-          <Link
-            key={category.id}
-            to={process.env.PUBLIC_URL + Pages.HomePage + category.id}
-          >
-            <div className="card bordered shadow-2xl hover:shadow-lg">
-              <figure>
-                <img
-                  src={process.env.PUBLIC_URL + category.img}
-                  alt={category.name}
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title text-center">{category.name}</h2>
+interface IParamTypes {
+  id: string;
+}
+
+const Home: React.FC<IHomeProps> = ({ categories }) => {
+  const { id } = useParams<IParamTypes>();
+
+  const pId = Number(id ?? 0);
+  console.log(id, pId);
+
+  return (
+    <Layout>
+      <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 md:grid-col-6">
+        {categories
+          .filter((c) => c.parentId === pId)
+          .map((category) => (
+            <Link
+              key={category.id}
+              to={process.env.PUBLIC_URL + Pages.HomePage + category.id}
+            >
+              <div className="card bordered shadow-2xl hover:shadow-lg">
+                <figure>
+                  <img
+                    src={process.env.PUBLIC_URL + category.img}
+                    alt={category.name}
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title text-center">{category.name}</h2>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
-    </div>
-  </Layout>
-);
+            </Link>
+          ))}
+      </div>
+    </Layout>
+  );
+};
 
 const mapStateToProps = (state: ApplicationState) => ({
   categories: state.data ? state.data.categories : [],
