cbf30d2fd204a560626547577606c6a362a04c82
mohsen <mohsen1299@gmail.com>
2021-03-29 13:55:01 +0430

    use layout in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import Layout from "../components/Layout";
 import { ICategory } from "../shares/Interfaces";
-import { Pages } from "../shares/URLs";
 import { ApplicationState } from "../store";
 
 interface IHomeProps {
@@ -18,82 +17,65 @@ const Home: React.FC<IHomeProps> = ({ categories }) => {
   const [searchText, SetSearchText] = React.useState<string>();
 
   return (
-    <div>
-      <main>
-        <div className={"flex " + (searchText ? "flex-row" : "flex-col")}>
-          <div className="max-w-xs m-auto">
-            <figure>
-              <img
-                src={process.env.PUBLIC_URL + "/img/national-park.png"}
-                alt="رامسر اپ"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-center vazir-font">رامسر اپ</h2>
-            </div>
-          </div>
-          <div className="w-2/3 md:max-w-sm m-auto">
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="جستجو"
-                value={searchText}
-                onChange={(e) => {
-                  SetSearchText(e.target.value);
-                }}
-                className="input input-info input-bordered vazir-font"
-              />
-              <label className="label">
-                <span className="label-text-alt vazir-font">
-                  مثل کلیدساز یا تاکسی یا بیمارستان
-                </span>
-              </label>
-            </div>
+    <Layout>
+      <div className={"flex " + (searchText ? "flex-row" : "flex-col")}>
+        <div className="max-w-xs m-auto">
+          <figure>
+            <img
+              src={process.env.PUBLIC_URL + "/img/national-park.png"}
+              alt="رامسر اپ"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-center vazir-font">رامسر اپ</h2>
           </div>
         </div>
+        <div className="w-2/3 md:max-w-sm m-auto">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="جستجو"
+              value={searchText}
+              onChange={(e) => {
+                SetSearchText(e.target.value);
+              }}
+              className="input input-info input-bordered vazir-font"
+            />
+            <label className="label">
+              <span className="label-text-alt vazir-font">
+                مثل کلیدساز یا تاکسی یا بیمارستان
+              </span>
+            </label>
+          </div>
+        </div>
+      </div>
 
-        <div className="flex flex-wrap justify-evenly">
-          {categories
-            .filter((c) => c.parentId === 0)
-            .map((category) => (
-              <Link
-                className="max-w-xs m-2"
-                key={category.id}
-                to={process.env.PUBLIC_URL}
-              >
-                <div className="card bordered shadow-2xl hover:shadow-lg">
-                  <figure>
-                    <img
-                      src={process.env.PUBLIC_URL + category.img}
-                      alt={category.name}
-                    />
-                  </figure>
-                  <div className="card-body">
-                    <h2 className="card-title text-center vazir-font">
-                      {category.name}
-                    </h2>
-                  </div>
+      <div className="flex flex-wrap justify-evenly">
+        {categories
+          .filter((c) => c.parentId === 0)
+          .map((category) => (
+            <Link
+              className="max-w-xs m-2"
+              key={category.id}
+              to={process.env.PUBLIC_URL}
+            >
+              <div className="card bordered shadow-2xl hover:shadow-lg">
+                <figure>
+                  <img
+                    src={process.env.PUBLIC_URL + category.img}
+                    alt={category.name}
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title text-center vazir-font">
+                    {category.name}
+                  </h2>
                 </div>
-              </Link>
-            ))}
-        </div>
-      </main>
-      <footer>
-        <div className="py-4 flex">
-          <ul className="menu items-stretch px-3 shadow-lg bg-base-100 bg-neutral-focus horizontal rounded-box m-auto vazir-font">
-            <li>
-              <a>صفحه نخست</a>
-            </li>
-            <li>
-              <a>ثبت مکان جدید</a>
-            </li>
-            <li>
-              <a>درباره ما</a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    </div>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </Layout>
   );
 };
 
