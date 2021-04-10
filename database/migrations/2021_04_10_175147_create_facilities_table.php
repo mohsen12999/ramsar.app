481f6f525e0567bc20d9096f545827202669f61b
mohsen <mohsen1299@gmail.com>
2021-04-10 23:11:41 +0430

    add data models

---
+++ database/migrations/2021_04_10_175147_create_facilities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('description');
+            $table->string('address');
+            $table->string('tel');
+            $table->string('tags');
+            $table->string('img');
+
+            $table->foreignId('category_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facilities');
+    }
+}
