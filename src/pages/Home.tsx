660964392c007614d53eb8caec2e959edb424c52
mohsen <mohsen1299@gmail.com>
2021-04-11 23:18:09 +0430

    removed unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Layout from "../components/Layout";
 import BreadCrumbs from "../components/BreadCrumbs";
 import FacilityThumb from "../components/FacilityThumb";
