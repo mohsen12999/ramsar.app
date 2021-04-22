f224fc31959791039be33118381a38d7ce3eb16f
mohsen <mohsen1299@gmail.com>
2021-04-22 19:29:13 +0430

    remove comments in home page

--- resources/ts/pages/Home.tsx
+++ resources/ts/pages/Home.tsx
@@ -133,40 +133,6 @@ const Home: React.FC<IHomeProps> = ({ categories, facilities }) => {
     );
 };
 
-// {
-//   const { id } = useParams<IParamTypes>();
-
-//   const pId = Number(id ?? 0);
-//   console.log(id, pId);
-
-//   return (
-//     <Layout>
-//       <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 md:grid-col-6">
-//         {categories
-//           .filter((c) => c.parentId === pId)
-//           .map((category) => (
-//             <Link
-//               key={category.id}
-//               to={process.env.PUBLIC_URL + Pages.HomePage + category.id}
-//             >
-//               <div className="card bordered shadow-2xl hover:shadow-lg">
-//                 <figure>
-//                   <img
-//                     src={process.env.PUBLIC_URL + category.img}
-//                     alt={category.name}
-//                   />
-//                 </figure>
-//                 <div className="card-body">
-//                   <h2 className="card-title text-center">{category.name}</h2>
-//                 </div>
-//               </div>
-//             </Link>
-//           ))}
-//       </div>
-//     </Layout>
-//   );
-// };
-
 const mapStateToProps = (state: ApplicationState) => ({
     categories: state.data ? state.data.categories : [],
     facilities: state.data ? state.data.facilities : [],
