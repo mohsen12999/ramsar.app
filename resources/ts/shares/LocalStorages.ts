c5771d7c298cf7d881334418e35ae9183988a9fb
mohsen <mohsen1299@gmail.com>
2021-04-23 11:44:14 +0430

    save and load category and facility

--- resources/ts/shares/LocalStorages.ts
+++ resources/ts/shares/LocalStorages.ts
@@ -1,11 +1,30 @@
-import { IDataState } from "../store/Data";
+// import { IDataState } from "../store/Data";
+import { ICategory, IFacility } from "./Interfaces";
 
-const Data = "Data";
+// const Data = "Data";
+const Categories = "Categories";
+const Facilities = "Facilities";
 
-export const SaveDataToLocalStorage = (data: IDataState) => {
-  localStorage.setItem(Data, JSON.stringify(data));
+// export const SaveDataToLocalStorage = (data: IDataState) => {
+//     localStorage.setItem(Data, JSON.stringify(data));
+// };
+
+// export const GetDataFromLocalStorage = (): IDataState => {
+//     return JSON.parse(localStorage.getItem(Data) as string);
+// };
+
+export const SaveCategoriesToLocalStorage = (categories: ICategory[]) => {
+    localStorage.setItem(Categories, JSON.stringify(categories));
 };
 
-export const GetDataFromLocalStorage = (): IDataState => {
-  return JSON.parse(localStorage.getItem(Data) as string);
+export const GetCategoriesFromLocalStorage = (): ICategory[] => {
+    return JSON.parse(localStorage.getItem(Categories) as string);
+};
+
+export const SaveFacilitiesToLocalStorage = (facilities: IFacility[]) => {
+    localStorage.setItem(Facilities, JSON.stringify(facilities));
+};
+
+export const GetFacilitiesFromLocalStorage = (): IFacility[] => {
+    return JSON.parse(localStorage.getItem(Facilities) as string);
 };
--- resources/ts/store/Data.ts
+++ resources/ts/store/Data.ts
@@ -2,25 +2,30 @@ import axios from "axios";
 import { Action, Reducer } from "redux";
 import { Status } from "../shares/Constants";
 import { ICategory, IFacility } from "../shares/Interfaces";
-import { GetDataFromLocalStorage } from "../shares/LocalStorages";
 import { DATA_URL } from "../shares/URLs";
 import { AppThunkAction } from "./";
 import { fakeCategories, fakeFacilities } from "../shares/FakeData";
+import {
+    GetCategoriesFromLocalStorage,
+    GetFacilitiesFromLocalStorage,
+    SaveCategoriesToLocalStorage,
+    SaveFacilitiesToLocalStorage,
+} from "../shares/LocalStorages";
 
 // -----------------
 // STATE - This defines the type of data maintained in the Redux store.
 
 export interface IDataState {
-  status: Status;
+    status: Status;
 
-  categories: ICategory[];
-  facilities: IFacility[];
+    categories: ICategory[];
+    facilities: IFacility[];
 }
 
 export enum DataActions {
-  DATA_REQUEST = "DATA_REQUEST",
-  DATA_SUCCESS = "DATA_SUCCESS ",
-  DATA_FAILURE = "DATA_FAILURE ",
+    DATA_REQUEST = "DATA_REQUEST",
+    DATA_SUCCESS = "DATA_SUCCESS ",
+    DATA_FAILURE = "DATA_FAILURE ",
 }
 
 // -----------------
@@ -29,8 +34,8 @@ export enum DataActions {
 // Use @typeName and isActionType for type detection that works even after serialization/deserialization.
 
 export interface ILoadData {
-  type: DataActions;
-  payload?: { message?: string; data?: any; error?: any };
+    type: DataActions;
+    payload?: { message?: string; data?: any; error?: any };
 }
 
 // Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
@@ -38,89 +43,100 @@ export interface ILoadData {
 export type KnownAction = ILoadData;
 
 export const actionCreators = {
-  loadData: (): AppThunkAction<KnownAction> => (dispatch, getState) => {
-    if (getState().data?.status === Status.LOADING) {
-      dispatch({
-        type: DataActions.DATA_FAILURE,
-        payload: { message: "we are in loading!" },
-      });
-      return false;
-    }
-
-    dispatch({ type: DataActions.DATA_REQUEST } as ILoadData);
-
-    try {
-      axios.get(DATA_URL).then((response) => {
-        if (response && response.data && response.data.success) {
-          dispatch({
-            type: DataActions.DATA_SUCCESS,
-            payload: {
-              message: "axios success get data",
-              data: response.data,
-            },
-          });
-          return true;
-        } else {
-          dispatch({
-            type: DataActions.DATA_FAILURE,
-            payload: { message: "axios not success", error: response },
-          });
-          return false;
+    loadData: (): AppThunkAction<KnownAction> => (dispatch, getState) => {
+        if (getState().data?.status === Status.LOADING) {
+            dispatch({
+                type: DataActions.DATA_FAILURE,
+                payload: { message: "we are in loading!" },
+            });
+            return false;
         }
-      });
-    } catch (error) {
-      dispatch({
-        type: DataActions.DATA_FAILURE,
-        payload: { message: "axios catch error", error: error },
-      });
-      return false;
-    }
-  },
+
+        dispatch({ type: DataActions.DATA_REQUEST } as ILoadData);
+
+        try {
+            axios.get(DATA_URL).then((response) => {
+                if (response && response.data && response.data.success) {
+                    dispatch({
+                        type: DataActions.DATA_SUCCESS,
+                        payload: {
+                            message: "axios success get data",
+                            data: response.data,
+                        },
+                    });
+                    return true;
+                } else {
+                    dispatch({
+                        type: DataActions.DATA_FAILURE,
+                        payload: {
+                            message: "axios not success",
+                            error: response,
+                        },
+                    });
+                    return false;
+                }
+            });
+        } catch (error) {
+            dispatch({
+                type: DataActions.DATA_FAILURE,
+                payload: { message: "axios catch error", error: error },
+            });
+            return false;
+        }
+    },
 };
 
 export const reducer: Reducer<IDataState> = (
-  state: IDataState | undefined,
-  incomingAction: Action
+    state: IDataState | undefined,
+    incomingAction: Action
 ): IDataState => {
-  if (state === undefined) {
-    return {
-      status: Status.INIT,
-      // TODO: Remove for production
-      categories: fakeCategories,
-      facilities: fakeFacilities,
-      //categories: [],
-      //facilities: [],
-    };
-  }
+    if (state === undefined) {
+        return {
+            status: Status.INIT,
+            // TODO: Remove for production
+            categories: fakeCategories,
+            facilities: fakeFacilities,
+            //categories: [],
+            //facilities: [],
+        };
+    }
 
-  const action = incomingAction as KnownAction;
+    const action = incomingAction as KnownAction;
 
-  switch (action.type) {
-    case DataActions.DATA_REQUEST:
-      const cachedData = GetDataFromLocalStorage();
+    switch (action.type) {
+        case DataActions.DATA_REQUEST:
+            const cachedCategories = GetCategoriesFromLocalStorage();
+            const cachedFacilities = GetFacilitiesFromLocalStorage();
 
-      return cachedData
-        ? { ...cachedData, status: Status.LOADING }
-        : { ...state, status: Status.LOADING };
+            const initState = { ...state, status: Status.LOADING };
+            if (cachedCategories) {
+                initState.categories = cachedCategories;
+            }
+            if (cachedFacilities) {
+                initState.facilities = cachedFacilities;
+            }
+            return initState;
 
-    case DataActions.DATA_FAILURE:
-      return { ...state, status: Status.FAILED };
+        case DataActions.DATA_FAILURE:
+            return { ...state, status: Status.FAILED };
 
-    case DataActions.DATA_SUCCESS:
-      if (action.payload && action.payload.data) {
-        const data = action.payload.data;
-        const currentState = { ...state };
-        if (data.categories) {
-          currentState.categories = data.categories;
-        }
-        if (data.facilities) {
-          currentState.facilities = data.facilities;
-        }
-        return { ...currentState, status: Status.SUCCEEDED };
-      }
-      return { ...state, status: Status.SUCCEEDED };
+        case DataActions.DATA_SUCCESS:
+            if (action.payload && action.payload.data) {
+                const data = action.payload.data;
+                const currentState = { ...state };
+                if (data.categories) {
+                    currentState.categories = data.categories;
+                    SaveCategoriesToLocalStorage(data.categories);
+                }
+                if (data.facilities) {
+                    currentState.facilities = data.facilities;
+                    SaveFacilitiesToLocalStorage(data.facilities);
+                }
+                return { ...currentState, status: Status.SUCCEEDED };
+            }
+            return { ...state, status: Status.SUCCEEDED };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
